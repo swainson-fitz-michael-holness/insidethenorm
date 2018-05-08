6a1dcbb78965f15af3c0fb17869adf3ac53a6ba6
exogenist <swainson.holness@gmail.com>
2018-05-08 00:37:35 -0600

    js update

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+function fx3dfollow() {
+    var point = document.querySelector(".image");
+    var canvas = document.querySelector(".container");
+    canvas.onmousemove = function (e) {
+        var delta = window.innerWidth / e.clientX;
+        var omega = 0.002 / delta;
+        var xAxis = omega - 0.001;
+
+        var sigma = window.innerHeight / e.clientY;
+        var beta = 0.002 / sigma;
+        var yAxis = beta - 0.001;
+
+        point.style.WebkitTransform = "matrix3d(1,0,0.00," + xAxis + ",0.00,1,0.00," + yAxis + ",0,0,1,0,0,0,0,1)";
+
+//        document.querySelector(".title").style.WebkitTransform = "matrix3d(1,0,0.00," + xAxis + ",0.00,1,0.00," + yAxis + ",0,0,1,0,0,0,0,1)";
+    }
+}
+
+fx3dfollow();
+
+
 
