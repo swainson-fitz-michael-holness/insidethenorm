ae0bab5c55e98387a86d2740d64b4890764ecc2a
exogenist <swainson.holness@gmail.com>
2018-08-11 15:13:53 -0600

    firebase init

--- prototype-login-v1/src/components/Login.js
+++ prototype-login-v1/src/components/Login.js
@@ -1,7 +1,43 @@
 import React, { Component } from 'react';
 import $ from "jquery";
 import '../App.css';
+import firebase from 'firebase';
 
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyCP4EMBCFQFw2VpqGIpTC_QRSbTi8DNyhw",
+    authDomain: "project-manager-7e24a.firebaseapp.com",
+    databaseURL: "https://project-manager-7e24a.firebaseio.com",
+    projectId: "project-manager-7e24a",
+    storageBucket: "project-manager-7e24a.appspot.com",
+    messagingSenderId: "260315914640"
+};
+firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+      alert('working');
+  } else {
+    // No user is signed in.
+      alert('signed out')
+  }
+});
+
+const login = () => {
+    const userEmail = document.getElementById('user').value;
+    const userPassword = document.getElementById('password').value;
+
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        alert(errorCode+' :: '+ errorMessage);
+        // ...
+    });
+}
 
 class Login extends Component {
   render() {
@@ -18,7 +54,7 @@ class Login extends Component {
                 </div>
 
                 <div className="login-btn">
-                    <button  type="button" class="btn btn-primary">
+                    <button onClick={login} type="button" class="btn btn-block btn-danger">
                     Submit
                     </button>
                 </div>
